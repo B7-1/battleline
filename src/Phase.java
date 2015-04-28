913ede5edd5df36794caff529d9598463b1f488e
Taichi Tomioka <mys.itachi@gmail.com>
2015-04-29 02:13:15 +0900

    各フェイズクラス（ステートメント図を参照）を追加。
    Stateパターンを適用した。

---
+++ src/Phase.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public interface Phase {
+	
+}
+
+class OrderPhase implements Phase {
+
+}
+
+class DealPhase implements Phase {
+
+}
+
+class SelectPhase implements Phase {
+
+}
+
+class StationPhase implements Phase {
+
+}
+
+class TacticsPhase implements Phase {
+
+}
+
+class DrawPhase implements Phase {
+
+}
+
+class ChangePlayerPhase implements Phase {
+
+}
+
+class GameOverPhase implements Phase {
+
+}
