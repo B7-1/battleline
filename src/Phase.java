4f1621e32b2db7378cebb4b95c8292ed45de151f
Taichi Tomioka <mys.itachi@gmail.com>
2015-04-29 02:14:56 +0900

    DealPhaseクラスにカードを配る処理を追加。
    コンストラクタでやってるけどあとで分ける。

--- src/Phase.java
+++ src/Phase.java
@@ -9,7 +9,37 @@ class OrderPhase implements Phase {
 }
 
 class DealPhase implements Phase {
+	DealPhase(GameSystem s) {
+		// delete all cards
+		s.unitStack.clear();
+		s.tacticsStack.clear();
+		s.players.get(0).cards.clear();
+		s.players.get(1).cards.clear();
 
+		// create unit card and shuffle stack
+		for (int suit = 0; suit < 6; suit++) {
+			for (int number = 1; number <= 10; number++) {
+				s.unitStack.add(new UnitCard(suit, number));
+			}
+		}
+		Collections.shuffle(s.unitStack);
+
+		// create tactics card and shuffle stack
+		for (Tactics t : Tactics.values()) {
+			s.tacticsStack.add(new TacticsCard(t));
+		}
+		Collections.shuffle(s.tacticsStack);
+
+		// deal cards to players
+		for (int i = 0; i < 7; i++) {
+			UnitCard c = s.unitStack.pop();
+			s.players.get(0).cards.add(c);
+		}
+		for (int i = 0; i < 7; i++) {
+			UnitCard c = s.unitStack.pop();
+			s.players.get(1).cards.add(c);
+		}
+	}
 }
 
 class SelectPhase implements Phase {
