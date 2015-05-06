c9960f970fa44698bed3ccc6848209e240b8ff23
Taichi Tomioka <mys.itachi@gmail.com>
2015-05-06 22:51:52 +0900

    戦術カード込みの判定処理を削除。
    やっぱり戦術カードの処理はstrengthOfSquadでやることにする。
    モラル戦術カードは候補の中から最大の点数が得られるものを選択するので、総当りでカードを適用して点数を計算する。

--- src/JudgePhase.java
+++ src/JudgePhase.java
@@ -8,40 +8,34 @@ public class JudgePhase implements Phase {
         s.selectionArea = Area.None;
     }
 
-    boolean isSkirmish(List<Card> cards) {
-        // TODO: add cases for tactics cards.
-
+    boolean isSkirmish(List<UnitCard> cards) {
         OptionalInt max = cards.stream()
-                .mapToInt((c) -> ((UnitCard)c).number)
+                .mapToInt(c -> c.number)
                 .max();
         OptionalInt min = cards.stream()
-                .mapToInt((c) -> ((UnitCard)c).number)
+                .mapToInt(c -> c.number)
                 .min();
 
         return max.getAsInt() - min.getAsInt() == 2;
     }
 
-    boolean isBattalion(List<Card> cards) {
-        // TODO: add cases for tactics cards.
-
+    boolean isBattalion(List<UnitCard> cards) {
         OptionalInt max = cards.stream()
-                .mapToInt((c) -> ((UnitCard)c).suit)
+                .mapToInt(c -> c.suit)
                 .max();
         OptionalInt min = cards.stream()
-                .mapToInt((c) -> ((UnitCard)c).suit)
+                .mapToInt(c -> c.suit)
                 .min();
 
         return max.getAsInt() == min.getAsInt();
     }
 
-    boolean isPhalanx(List<Card> cards) {
-                // TODO: add cases for tactics cards.
-
+    boolean isPhalanx(List<UnitCard> cards) {
         OptionalInt max = cards.stream()
-                .mapToInt((c) -> ((UnitCard)c).number)
+                .mapToInt(c -> c.number)
                 .max();
         OptionalInt min = cards.stream()
-                .mapToInt((c) -> ((UnitCard)c).number)
+                .mapToInt(c -> c.number)
                 .min();
 
         return max.getAsInt() == min.getAsInt();
@@ -49,15 +43,17 @@ public class JudgePhase implements Phase {
 
     int strengthOfSquad(List<Card> cards) {
         int str = 0;
+        List<UnitCard> unitcards = new ArrayList<UnitCard>();
 
         for (Card c : cards) {
             assert (c instanceof UnitCard);
             str += ((UnitCard)c).number * 10;
+            unitcards.add((UnitCard)c);
         }
 
-        if (isSkirmish(cards)) str += 10000;
-        if (isBattalion(cards)) str += 20000;
-        if (isPhalanx(cards)) str += 25000;
+        if (isSkirmish(unitcards)) str += 10000;
+        if (isBattalion(unitcards)) str += 20000;
+        if (isPhalanx(unitcards)) str += 25000;
 
         return str;
     }
