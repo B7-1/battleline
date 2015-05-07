38733826dcd0964a3dc7c54cc31ab413decff8c6
Taichi Tomioka <mys.itachi@gmail.com>
2015-05-07 11:09:59 +0900

    重複するカードについてスカーミッシュ判定のテストを追加。

--- test/SkirmishTest.java
+++ test/SkirmishTest.java
@@ -3,12 +3,16 @@ import org.junit.Test;
 import java.util.*;
 
 public class SkirmishTest {
-	UnitCard c(int s, int n) {
-		return new UnitCard(s, n);
-	}
+	List<UnitCard> squad(Integer... cards) {
+		List<UnitCard> squad = new ArrayList<UnitCard>();
 
-	List<Card> squad(Card... cards) {
-		return Arrays.asList(cards);
+		for (Integer i : cards) {
+			int suit = i / 10;
+			int number = (i % 10) > 0 ? (i % 10) : 10;
+			squad.add(new UnitCard(suit, number));
+		}
+
+		return squad;
 	}
 
 	JudgePhase phase;
@@ -19,19 +23,36 @@ public class SkirmishTest {
 
 	@Test
 	public void skirmish() {
-		assertTrue(phase.isSkirmish(squad(c(0,1), c(0,2), c(0,3))));
-		assertTrue(phase.isSkirmish(squad(c(0,5), c(0,6), c(0,7))));
-		assertTrue(phase.isSkirmish(squad(c(0,8), c(0,9), c(0,10))));
+		assertTrue(phase.isSkirmish(squad(1, 2, 3)));
+		assertTrue(phase.isSkirmish(squad(5, 6, 7)));
+		assertTrue(phase.isSkirmish(squad(8, 9, 10)));
 	}
 
 	@Test
 	public void skirmish_leapnumber() {
-		assertFalse(phase.isSkirmish(squad(c(0,2), c(0,4), c(0,6))));
+		assertFalse(phase.isSkirmish(squad(2, 4, 6)));
 	}
 
 	@Test
 	public void skirmish_shuffle() {
-		assertTrue(phase.isSkirmish(squad(c(0,5), c(0,4), c(0,6))));
-		assertTrue(phase.isSkirmish(squad(c(0,3), c(0,2), c(0,1))));
+		assertTrue(phase.isSkirmish(squad(5, 4, 6)));
+		assertTrue(phase.isSkirmish(squad(3, 2, 1)));
+	}
+
+	@Test
+	public void skirmish_duplicate() {
+		assertFalse(phase.isSkirmish(squad(1, 2, 2)));
+		assertFalse(phase.isSkirmish(squad(2, 2, 3)));
+		assertFalse(phase.isSkirmish(squad(1, 1, 3)));
+		assertFalse(phase.isSkirmish(squad(1, 3, 3)));
+	}
+
+	@Test
+	public void skirmish_forth() {
+		assertTrue(phase.isSkirmish(squad(1, 2, 3, 4)));
+		assertTrue(phase.isSkirmish(squad(3, 4, 6, 5)));
+
+		assertFalse(phase.isSkirmish(squad(1, 2, 3, 8)));
+		assertFalse(phase.isSkirmish(squad(1, 2, 3, 3)));
 	}
 }
